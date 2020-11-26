ef5a3349544aa06d6ef634037d45f44144ff329b
caya <caya@askanna.io>
2020-11-26 11:34:38 +0100

    pdf extraction

--- sitemap2.py
+++ sitemap2.py
@@ -8,28 +8,32 @@ f = open("xmlfiles.txt", "a")
 def get_sitemap():
     with open("get_consultancy_firms.txt", "r") as file:
         urls = file.readlines()
+        sitemap = ''
         for url in urls:
             url = url.strip('\n')
             if requests.get(url + 'sitemap').status_code == 200:
-                return requests.get(url + 'sitemap')
+                sitemap += requests.get(url + 'sitemap').text
             elif requests.get(url + 'sitemap.xml').status_code == 200:
-                return requests.get(url + 'sitemap.xml')
+                sitemap += requests.get(url + 'sitemap.xml').text
             elif requests.get(url + 'sitemap_index.xml').status_code == 200:
-                return requests.get(url + 'sitemap_index.xml')
+                sitemap += requests.get(url + 'sitemap_index.xml').text
             elif requests.get(url + 'sitemap-index.xml').status_code == 200:
-                return requests.get(url + 'sitemap-index.xml')
+                sitemap += requests.get(url + 'sitemap-index.xml').text
             elif requests.get(url + 'special-pages/google-site-map').status_code == 200:
-                return requests.get(url + 'special-pages/google-site-map')
+                sitemap += requests.get(url + 'special-pages/google-site-map').text
             elif requests.get(url + 'sitemap-list').status_code == 200:
-                return requests.get(url + 'sitemap-list')
+                sitemap += requests.get(url + 'sitemap-list').text
             elif requests.get(url + 'sitemap.aspx').status_code == 200:
-                return requests.get(url + 'sitemap.aspx')
+                sitemap += requests.get(url + 'sitemap.aspx').text
             elif requests.get(url + 'sitemapGEHC.xml').status_code == 200:
-                return requests.get(url + 'sitemapGEHC.xml')
+                sitemap += requests.get(url + 'sitemapGEHC.xml').text
             elif requests.get(url + 'sitemap_en.xml').status_code == 200:
-                return requests.get(url + 'sitemap_en.xml')
+                sitemap += requests.get(url + 'sitemap_en.xml').text
             else:
                 print('Unable to fetch sitemap: %s.' % url)
+                pass
+
+    return sitemap
 
 
 def process_sitemap(s):
@@ -58,7 +62,8 @@ def parse_sitemap(s):
         candidate = sitemap.pop()
 
         if is_sub_sitemap(candidate):
-            sub_sitemap = get_sitemap()
+            sub_sitemap = requests.get(candidate).text
+            # sub_sitemap = get_sitemap()
             for i in process_sitemap(sub_sitemap):
                 sitemap.append(i)
         else:
@@ -66,30 +71,30 @@ def parse_sitemap(s):
 
     return result
 
-#
-# def pdf_searcher():
-#     """get request on all urls, checks for pdf files and add them to folder"""
-#     folder_location = r'E:\web_scraping'
-#     if not os.path.exists(folder_location): os.mkdir(folder_location)
-#
-#     with open("xmlfiles.txt", "r") as file:
-#         urls = file.readlines()
-#         for url in urls:
-#             url = url.strip('\n')
-#             response = requests.get(url)
-#             print(response)
-#             soup = BeautifulSoup(response.text, "html.parser")
-#             for link in soup.select("a[href$='.pdf']"):
-#                 # Name the pdf files using the last portion of each link which are unique in this case
-#                 filename = os.path.join(folder_location, link['href'].split('/')[-1])
-#                 with open(filename, 'wb') as file:
-#                     file.write(requests.get(urljoin(url, link['href'])).content)
+
+def pdf_searcher(urls):
+    """get request on all urls, checks for pdf files and add them to folder"""
+    folder_location = r'E:\consultancy_firm_pdfs'
+    if not os.path.exists(folder_location): os.mkdir(folder_location)
+
+    # with open("xmlfiles.txt", "r") as file:
+    #     urls = file.readlines()
+    for url in urls:
+        url = url.strip('\n')
+        response = requests.get(url)
+        print(response)
+        soup = BeautifulSoup(response.text, "html.parser")
+        for link in soup.select("a[href$='.pdf']"):
+            # Name the pdf files using the last portion of each link which are unique in this case
+            filename = os.path.join(folder_location, link['href'].split('/')[-1])
+            with open(filename, 'wb') as file:
+                file.write(requests.get(urljoin(url, link['href'])).content)
 
 
 def main():
     sitemap = get_sitemap()
-    url = '\n'.join(parse_sitemap(sitemap))
-    # pdf_searcher()
+    urls = parse_sitemap(sitemap)
+    pdf_searcher(urls)
 
 
 if __name__ == '__main__':
