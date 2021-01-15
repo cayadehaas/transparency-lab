58d172fa92b53c070e5116adde06875a24243512
caya <caya@askanna.io>
2021-01-15 11:05:41 +0100

    added terms in title

--- white_paper_extracter.py
+++ white_paper_extracter.py
@@ -13,23 +13,35 @@ def title_searcher():
     if not os.path.exists(non_white_paper_folder_location): os.mkdir(non_white_paper_folder_location)
 
     white_paper = ['whitepaper', 'White Paper', 'Whitepaper', 'White paper', 'white-paper', 'white_paper', 'White_paper', 'White_Paper', 'white paper']
-    terms = ['affidavit', 'applicant', 'confidential', 'businessman', 'disbursement', 'retainer']
+    terms_in_pdfs = ['affidavit', 'applicant', 'confidential', 'businessman', 'disbursement', 'retainer']
+    terms_in_title = ['affidavit', 'cv', 'CV', 'Cv', 'brochure', 'Brochure', 'CaseStudy', 'Case Study',
+                      'Case-Study', 'Case-study', 'case-study', 'case study', 'Case_study', 'Case_Study',
+                      'case_study', 'annual', 'Annual', 'Survey' 'survey', 'Guide', 'guide', 'Newsletter',
+                      'newsletter' 'summary', 'Summary' 'Highlights', 'RiskStudy', 'Riskstudy', 'riskstudy',
+                      'risk_study', 'Risk_Study', 'risk-study', 'Risk-Study', 'Webinar', 'webinar', 'agenda']
 
     for root, dirs, files in os.walk('/Users/cayadehaas/PycharmProjects/Transparency-Lab/pdfs_consultancy_firms',
                                      topdown=False):
         directory = root.split('/')
 
         for filename in files:
-            # Check if white paper in title
+            # Check if white paper in title and add to white paper directory
             if any(x in filename for x in white_paper):
                 firm = directory[-1]
                 path = os.path.join(white_paper_folder_location, firm)
                 if not os.path.exists(path): os.makedirs(path)
                 os.rename("pdfs_consultancy_firms/" + firm + '/' + filename,
                           path + '/' + filename)
+            elif any(x in filename for x in terms_in_title):
+                # Check if terms in title and add to non white paper directory
+                firm = directory[-1]
+                path = os.path.join(non_white_paper_folder_location, firm)
+                if not os.path.exists(path): os.makedirs(path)
+                os.rename("pdfs_consultancy_firms/" + firm + '/' + filename,
+                          path + '/' + filename)
             else:
                 try:
-                    #if white paper in text:
+                    # if white paper in text:
                     pdf = open(root + '/' + filename, 'rb')
                     file_reader = PyPDF2.PdfFileReader(pdf)
 
@@ -44,8 +56,8 @@ def title_searcher():
                             os.rename("pdfs_consultancy_firms/" + firm + '/' + filename,
                                       path + '/' + filename)
 
-                        for term in terms:
-                            #check if file contains non-white paper terms and add to directory
+                        for term in terms_in_pdfs:
+                            # Check if file contains non-white paper terms and add to directory
                             if term in text:
                                 firm = directory[-1]
                                 path = os.path.join(non_white_paper_folder_location, firm)
@@ -61,7 +73,7 @@ def title_searcher():
                         if not os.path.exists(path): os.makedirs(path)
                         os.rename("pdfs_consultancy_firms/" + firm + '/' + filename,
                                   path + '/' + filename)
-                    for term in terms:
+                    for term in terms_in_pdfs:
                         # check if file contains non-white paper terms and add to directory
                         if term in readable_text:
                             firm = directory[-1]
