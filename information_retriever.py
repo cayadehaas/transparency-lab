88c86bf816fa64c85642fe302adf8ecddb504c90
caya <caya@askanna.io>
2021-02-04 10:37:18 +0100

    count images

--- information_retriever.py
+++ information_retriever.py
@@ -4,12 +4,15 @@ import textract, re
 import pdftotext
 import pandas as pd
 import os
+from PyPDF2 import PdfFileReader
+import pdftitle
 import glob
-# brandname, url, title , authors, nbr of pages, nbr of words, nbr of illustrations, nbr of graphs, nbr of numbers/precentages
+# brandname, url, title , authors, nbr of pages, nbr of words, nbr of illustrations, nbr of graphs, nbr of numbers/percentages
 
 with open('white_papers.csv', 'w', newline='') as file:
+    image_counter = 0
     writer = csv.writer(file)
-    writer.writerow(["BRANDNAME", "URL", "TITLE", "AUTHORS", "NBR. OF PAGES", "NBR. OF CHARACTERS"])
+    writer.writerow(["BRANDNAME", "URL", "FILENAME", "TITLE", "AUTHORS", "NBR. OF PAGES", "NBR. OF CHARACTERS", "NBR. OF IMAGES", "NBR. OF NUMBERS"])
 
     for root, dirs, files in os.walk('/Users/cayadehaas/PycharmProjects/Transparency-Lab/pdfs_consultancy_firms/WHITEPAPERS',
                                  topdown=False):
@@ -22,26 +25,51 @@ with open('white_papers.csv', 'w', newline='') as file:
                 firm_name = firm.split(',')
                 if '.pdf' in file:
                     with open(root + "/" + file, "rb") as f:
+                        FILENAME = file
                         pdf = pdftotext.PDF(f)
                         nr_of_pages = len(pdf)
+
+                    pdf_toread = PdfFileReader(open(root + "/" + file, "rb"))
+                    pdf_info = pdf_toread.getDocumentInfo()
+                    AUTHORS = pdf_info.author
+                    TITLE = pdf_info.title
+                    for i in range(pdf_toread.numPages - 1):
+                        page0 = pdf_toread.getPage(i + 1)
+                        if '/XObject' in page0['/Resources']:
+                            xObject = page0['/Resources']['/XObject'].getObject()
+                            for obj in xObject:
+                                if xObject[obj]['/Subtype'] == '/Image':
+                                    image_counter += 1
+                    IMAGES = image_counter
+
                     nr_of_characters = 0
                     rawText = parser.from_file(root + '/' + file)
+
+                    clean_text = rawText['content'].replace('\n', '')
+                    NUMBERS = sum(c.isdigit() for c in clean_text)
+
                     try:
                         list_of_nr = rawText['metadata']['pdf:charsPerPage']
                         for nr_words_per_page in list_of_nr:
                             nr_of_characters += int(nr_words_per_page)
                         nr_of_characters = nr_of_characters
+
+
+
                         rawList = rawText['content'].splitlines()
+                        while '' in rawList: rawList.remove('')
                     except:
                         continue
                     try:
                         if directory[7] == firm_name[0]:
                             BRANDNAME = directory[7]
                             URL = firm_name[-1].strip('\n')
-                            print(file, BRANDNAME, URL, nr_of_pages, nr_of_characters)
-                            writer.writerow([BRANDNAME, URL, "TITLE", "AUTHORS", nr_of_pages, nr_of_characters])
+                            print(file, BRANDNAME, URL, nr_of_pages, nr_of_characters, IMAGES, NUMBERS)
+                            writer.writerow([BRANDNAME, URL, FILENAME, TITLE, AUTHORS, nr_of_pages, nr_of_characters, IMAGES, NUMBERS])
                     except:
                         continue
+
+            image_counter = 0
 
 read_file = pd.read_csv(r'white_papers.csv')
 read_file.to_excel(r'whitepapers.xlsx', index=None, header=True)
