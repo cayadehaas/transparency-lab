b68af225e86c39dc95f3436fe1e35c6f7aca4aee
caya <caya@askanna.io>
2021-04-14 21:46:31 +0200

    tf-idf per topic

--- tf-idf/tfidf_per_topic.py
+++ tf-idf/tfidf_per_topic.py
@@ -17,33 +17,33 @@ pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/pytesseract'
 
 def pdf2text(pdf):
     '''Iterate over pages and extract text'''
-    images = convert_from_path(pdf)
-    NBR_OF_PAGES = len(images)
-    complete_PDF_List = []
-    page = 0
-    text = ''
-    for i in range(len(images)):
-        print(f'Working on File:{pdf} Page No: {page + 1}')
-        extractedInformation = pytesseract.image_to_string(images[i])
-        extractedInformation = extractedInformation.replace('\n', '')
-        n_sentences = re.sub(r'([a-z])([A-Z])', r'\1 \2', extractedInformation)
-        g_sentences = re.sub(r'(?<=[.,?!%:])(?=[^\s])', r' ', n_sentences)  # adds whitespace after . and , 
-        y_sentences = re.sub(r'\\xa0', r' ', g_sentences)  # changes \xa0 to whitespace 
-        b_sentences = re.sub(r'% 20', r'', y_sentences)  # changes %20 to whitespace 
-        t_best_sentences = re.sub(r'% 40', r'', b_sentences)  # changes %40 to whitespace 
-        gr_sentences = re.sub(r'[-]', r'', t_best_sentences)
-        fr_sentences = re.sub(r'([0-9])([a-zA-Z])', r'\1 \2', gr_sentences)  # adds whitespace between number and letters 
-        li = re.sub(r"((www\.) ([a-z]+\.) (com))", r" \2\3\4 ", fr_sentences)  # remove space between link 
-        li2 = re.sub(r"(([A-Za-z]+@[a-z]+\.) (com))", r" \2\3 ", li)
-        text += li2
-        page + 1
-
-    return text
+    # images = convert_from_path(pdf)
+    # NBR_OF_PAGES = len(images)
+    # complete_PDF_List = []
+    # page = 0
     # text = ''
-    # for i in range(pdf.getNumPages()):
-    #     page = pdf.getPage(i)
-    #     text = text + page.extractText()
+    # for i in range(len(images)):
+    #     print(f'Working on File:{pdf} Page No: {page + 1}')
+    #     extractedInformation = pytesseract.image_to_string(images[i])
+    #     extractedInformation = extractedInformation.replace('\n', '')
+    #     n_sentences = re.sub(r'([a-z])([A-Z])', r'\1 \2', extractedInformation)
+    #     g_sentences = re.sub(r'(?<=[.,?!%:])(?=[^\s])', r' ', n_sentences)  # adds whitespace after . and , 
+    #     y_sentences = re.sub(r'\\xa0', r' ', g_sentences)  # changes \xa0 to whitespace 
+    #     b_sentences = re.sub(r'% 20', r'', y_sentences)  # changes %20 to whitespace 
+    #     t_best_sentences = re.sub(r'% 40', r'', b_sentences)  # changes %40 to whitespace 
+    #     gr_sentences = re.sub(r'[-]', r'', t_best_sentences)
+    #     fr_sentences = re.sub(r'([0-9])([a-zA-Z])', r'\1 \2', gr_sentences)  # adds whitespace between number and letters 
+    #     li = re.sub(r"((www\.) ([a-z]+\.) (com))", r" \2\3\4 ", fr_sentences)  # remove space between link 
+    #     li2 = re.sub(r"(([A-Za-z]+@[a-z]+\.) (com))", r" \2\3 ", li)
+    #     text += li2
+    #     page += 1
+    #
     # return text
+    text = ''
+    for i in range(pdf.getNumPages()):
+        page = pdf.getPage(i)
+        text = text + page.extractText()
+    return text
 
 def stem_tokenize(document):
     '''return stemmed words longer than 2 chars and all alpha'''
@@ -61,8 +61,8 @@ def build_corpus_from_dir(dir_path):
         for name in filenames:
             f = os.path.join(root, name)
             if '.pdf' in name:
-                # pdf = PdfFileReader(f, "rb")
-                pdf = root + '/' + name
+                pdf = PdfFileReader(f, "rb")
+                # pdf = root + '/' + name
                 document = pdf2text(pdf)
                 corpus.append(document)
     return corpus
@@ -75,7 +75,9 @@ if __name__ == '__main__':
     indices = np.argsort(vectorizer.idf_)[::-1]
     features = vectorizer.get_feature_names()
     for doc in corpus:
-        breakpoint()
+        keywords = []
+        tf_idf_scores = []
+        scores = []
         doc = ' '.join(doc.split())
         # print(' '.join(doc.split())[500])
         tdm = vectorizer.transform([doc])
@@ -83,6 +85,15 @@ if __name__ == '__main__':
         episode = dense[0].tolist()[0]
         phrase_scores = [pair for pair in zip(range(0, len(episode)), episode) if pair[1] > 0]
         sorted_phrase_scores = sorted(phrase_scores, key=lambda t: t[1] * -1)
-        for phrase, score in [(features[word_id], score) for (word_id, score) in sorted_phrase_scores][:10]:
-           print('{0: <20} {1}'.format(phrase, score))
+        for phrase, score in [(features[word_id], score) for (word_id, score) in sorted_phrase_scores][:50]:
+            keywords.append(phrase)
+            tf_idf_scores.append('{0} {1}'.format(phrase, score))
+            scores.append(score)
+            print('{0: <20} {1}'.format(phrase, score))
         print()
+        tf_idf_scores_list = ', '.join(tf_idf_scores)
+        keywords_list = ', '.join(keywords)
+        average_tf_idf_score = sum(scores)/len(scores)
+
+
+
